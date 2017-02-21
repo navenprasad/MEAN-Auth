f6a6cc02f27aa324d561b530aae2eb0ef8506f7b
Naven Prasad <navenprasad25@gmail.com>
2017-02-21 18:51:59 +0800

    Flash done

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  name: String;
+  username: String;
+  email: String;
+  password: String;
 
-  constructor() { }
+  constructor(private validateService: ValidateService, private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit(){
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+
+    }
+    //Required Fields
+    if(!this.validateService.validateRegister(user)){
+      this.flashMessage.show("Please fill everything", {cssClass: 'alert-danger', timeout:10000});
+      return false;
+    }
+
+    if(!this.validateService.validateEmail(user.email)){
+      this.flashMessage.show("Wrong email format", {cssClass: 'alert-danger', timeout:10000});
+      return false;
+    }
+
+
+
+
+
+
+
+
+
+
   }
 
 }
