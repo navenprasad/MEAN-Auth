7e9cf60970ec38c92027770e7c601f95f8b1715f
Naven Prasad <navenprasad25@gmail.com>
2017-02-22 19:38:09 +0800

    Login/Logout done

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
 import {FlashMessagesService} from 'angular2-flash-messages';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -13,7 +16,12 @@ export class RegisterComponent implements OnInit {
   email: String;
   password: String;
 
-  constructor(private validateService: ValidateService, private flashMessage: FlashMessagesService) { }
+  constructor(
+    private validateService: ValidateService,
+    private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -32,10 +40,25 @@ export class RegisterComponent implements OnInit {
       return false;
     }
 
+    //Validate emai
     if(!this.validateService.validateEmail(user.email)){
       this.flashMessage.show("Wrong email format", {cssClass: 'alert-danger', timeout:10000});
       return false;
     }
+
+    //Register Users
+    this.authService.registerUser(user).subscribe(data =>{
+      if(data.success ){
+        this.flashMessage.show("You are now registered", {cssClass: 'alert-success', timeout:10000});
+        this.router.navigate(['/login']);
+      }else{
+        this.flashMessage.show("Somethign went wrong", {cssClass: 'alert-danger', timeout:10000});
+        this.router.navigate(['/register']);
+
+      }
+    });
+
+
 
 
 
